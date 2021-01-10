4f838ed2b248e284ec0fef8b913b3b21f6c26134
yannihilator <64179787+yannihilator@users.noreply.github.com>
2021-01-09 22:51:13 -0500

    ranks players by value, grouped by position in the output.

--- Program.cs
+++ Program.cs
@@ -11,8 +11,17 @@ namespace FPL_Forecaster
             //Console.WriteLine("Hello World! This is my FPL forecaster script");
             var controller = new Controller();
             controller.GetData();
-            DataService.Data.Players.GroupBy(x => x?.element_type);
-            DataService.Data.Players = DataService.Data.Players.OrderBy(x => x?.total_points).ToList();
+            var test = DataService.Data.Players.GroupBy(x => x?.element_type);
+            foreach (IGrouping<int?, Models.Player> group in test)
+            {
+                Console.WriteLine("\n\n\n");
+                Console.WriteLine(DataService.Enumerators.PlayerTypes.Where(x => x?.id == group.Key).FirstOrDefault()?.plural_name);
+                Console.WriteLine("****************************************************************");
+                foreach (Models.Player player in group.Where(y => y.total_points > 0).OrderByDescending(x => x?.total_points/x?.now_cost).ToList())
+                {
+                    Console.WriteLine($"{player.first_name} {player.second_name}: {player.total_points}");
+                }
+/*             DataService.Data.Players = DataService.Data.Players.OrderBy(x => x?.total_points).ToList();
             foreach (Models.Player player in DataService.Data.Players)
             {
                 Console.WriteLine($"{player.first_name} {player.second_name}: {player.total_points}");
@@ -20,6 +29,7 @@ namespace FPL_Forecaster
             foreach (Models.ManagerGameweek gameweekData in DataService.Data.ManagerData)
             {
                 Console.WriteLine($"Gameweek {gameweekData.@event} Total Points: {gameweekData.points}");
+            } */
             }
         }
     }
