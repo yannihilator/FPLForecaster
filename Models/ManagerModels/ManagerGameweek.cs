9209843da834db3a32651fb1afb35fa1800abfe2
yannihilator <64179787+yannihilator@users.noreply.github.com>
2021-01-13 17:43:04 -0500

    Great successssssss, chart is now showing. looks pretty decent too honestly woot woot. Gonna continue editing UI

--- Models/ManagerModels/ManagerGameweek.cs
+++ Models/ManagerModels/ManagerGameweek.cs
@@ -7,16 +7,16 @@ namespace FPLForecaster.Models.Manager
     public class ManagerGameweek
     {
         [JsonProperty("event")]
-        public int number { get; set; } 
-        public int points { get; set; } 
-        public int total_points { get; set; } 
-        public int rank { get; set; } 
-        public int rank_sort { get; set; } 
-        public int overall_rank { get; set; } 
-        public int bank { get; set; } 
-        public int value { get; set; } 
-        public int event_transfers { get; set; } 
-        public int event_transfers_cost { get; set; } 
-        public int points_on_bench { get; set; } 
+        public int? number { get; set; } 
+        public int? points { get; set; } 
+        public int? total_points { get; set; } 
+        public int? rank { get; set; } 
+        public int? rank_sort { get; set; } 
+        public int? overall_rank { get; set; } 
+        public int? bank { get; set; } 
+        public int? value { get; set; } 
+        public int? event_transfers { get; set; } 
+        public int? event_transfers_cost { get; set; } 
+        public int? points_on_bench { get; set; } 
     }
 }
