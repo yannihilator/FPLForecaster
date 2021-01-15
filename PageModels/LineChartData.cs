21f6f1429260174df8c4139f87ba039719eb96dd
yannihilator <64179787+yannihilator@users.noreply.github.com>
2021-01-14 22:58:46 -0500

    GOT A PARTIAL VIEW to work for chart.js line chart element. implementing currently

---
+++ PageModels/LineChartData.cs
@@ -0,0 +1,18 @@
+
+
+namespace FPLForecaster.Models
+{
+    public class LineChartData
+    {
+        public string chartId {get;set;}
+        public string chartTitle {get;set;}
+        public string xValuesJson {get;set;}
+        public string yValuesJson {get;set;}
+        public string xLabel {get;set;}
+        public string yLabel {get;set;}
+        public string hoverLabel {get;set;}
+        public bool hoverLabelBefore {get;set;}
+        public string hoverTitle {get;set;}
+        public bool hoverTitleBefore {get;set;}
+    }
+}
