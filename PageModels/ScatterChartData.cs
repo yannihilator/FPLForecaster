43eb5b185c53a7b9c8d8407afa1a353307df5102
yannihilator <64179787+yannihilator@users.noreply.github.com>
2021-01-23 16:47:56 -0500

    successfully implemented player ROI scatter chart in player analysis view

--- PageModels/ScatterChartData.cs
+++ PageModels/ScatterChartData.cs
@@ -6,12 +6,9 @@ namespace FPLForecaster.Models
     {
         public string chartId {get;set;}
         public string chartTitle {get;set;}
-        public string xLabelsJson {get;set;}
-        public string yValuesJson_1 {get;set;}
-        public string yValuesJson_2 {get;set;}
+        public string dataJson {get;set;}
         public string xLabel {get;set;}
-        public string yLabel_1 {get;set;}
-        public string yLabel_2 {get;set;}
+        public string yLabel {get;set;}
         public string height_pixels {get;set;}
     }
 }
