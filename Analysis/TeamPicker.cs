e63cc7c2f4ca5489fdd7f901dad304219aa5024b
yannihilator <64179787+yannihilator@users.noreply.github.com>
2021-01-25 17:54:50 -0500

    Initial version of AI team picker is complete. Will continue to refine. Working on displaying picks nicely on UI

--- Analysis/TeamPicker.cs
+++ Analysis/TeamPicker.cs
@@ -13,16 +13,69 @@ namespace FPLForecaster.Analysis
 
             //gets players that 
             List<Player> playersByRoi = DataService.Data.Players != null && DataService.Data.Players.Count > 0 ? 
-                DataService.Data.Players.OrderBy(x => Convert.ToDouble(x.total_points/x.now_cost)).ToList() : null;
+                DataService.Data.Players.OrderByDescending(x => Convert.ToDouble(x.total_points/x.now_cost)).ToList() : null;
             List<Player> playersByPoints = DataService.Data.Players != null && DataService.Data.Players.Count > 0 ?
-                DataService.Data.Players.OrderBy(x => x.total_points).ToList() : null;
-
+                DataService.Data.Players.OrderByDescending(x => x.total_points).ToList() : null;
+                
             //cost variable
             double teamCost = 100.0;
+            
+            //counters
+            int goalkeeperCount = 2;
+            int defenderCount = 5;
+            int midfielderCount = 5;
+            int forwardCount = 3;
+            int counter = 0;
 
             if (playersByRoi != null && playersByPoints != null && playersByRoi.Count > 0)
             {
-                
+                while (chosenTeam.Count < 15)
+                {
+                    var playerList = counter % 2 == 0 ? playersByPoints : playersByRoi;
+                    var playerTypes = DataService.Enumerators.PlayerTypes;
+                    if (forwardCount > 0)
+                    {      
+                        chosenTeam.Add(playerList.Where(x => 
+                            playerTypes.Where(y => y.id == x.element_type).FirstOrDefault()?.singular_name?.ToLower() == "forward"
+                            && teamCost - (double)x.now_cost/10 > 0
+                            && x.status == "a"
+                            && chosenTeam.Contains(x) == false).FirstOrDefault());
+                        forwardCount--;
+                    }
+                    if (midfielderCount > 0)
+                    {
+                        chosenTeam.Add(playerList.Where(x => 
+                            playerTypes.Where(y => y.id == x.element_type).FirstOrDefault()?.singular_name?.ToLower() == "midfielder"
+                            && teamCost - (double)x.now_cost/10 > 0
+                            && x.status == "a"
+                            && chosenTeam.Contains(x) == false).FirstOrDefault());
+                        midfielderCount--;
+                    }
+                    if (defenderCount > 0)
+                    {
+                        chosenTeam.Add(playerList.Where(x => 
+                            playerTypes.Where(y => y.id == x.element_type).FirstOrDefault()?.singular_name?.ToLower() == "defender"
+                            && teamCost - (double)x.now_cost/10 > 0
+                            && x.status == "a"
+                            && chosenTeam.Contains(x) == false).FirstOrDefault());
+                        defenderCount--;
+                    }
+                    if (goalkeeperCount > 0)
+                    {
+                        chosenTeam.Add(playerList.Where(x => 
+                            playerTypes.Where(y => y.id == x.element_type).FirstOrDefault()?.singular_name?.ToLower() == "goalkeeper"
+                            && teamCost - (double)x.now_cost/10 > 0
+                            && x.status == "a"
+                            && chosenTeam.Contains(x) == false).FirstOrDefault());
+                        goalkeeperCount--;
+                    }
+                    counter++;
+                }
+            }
+
+            foreach (Player player in chosenTeam)
+            {
+                player.playerDetails = DataService.Controller.playerClient.GetPlayerDetails(player.id).Result;
             }
 
             return chosenTeam;
